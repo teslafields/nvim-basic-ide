c78f2bedffab8235a05f42e90139cf7c432300cd
Benhur Tessele <benhur.tessele@satcube.com>
2026-08-17 09:14:45 +0200

    clangd settings

---
+++ lua/lsp/settings/clangd.lua
@@ -0,0 +1,8 @@
+return {
+  settings = {
+    clangd = {
+      filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+      root_markers = { '.clangd', '.clang-format', 'compile_commands.json', '.git' },
+    }
+  }
+}
