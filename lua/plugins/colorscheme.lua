04e046dbcc007b03f5a01667ddcde931849bb06c
Benhur Tessele <benhur.tessele@est.tech>
2026-09-02 12:52:17 +0200

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,9 +9,6 @@ local M = { {
   name = 'gruvbox-material',
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    pcall(vim.cmd.colorscheme, "gruvbox")
-  end
 },
 {
   "catppuccin/nvim",
